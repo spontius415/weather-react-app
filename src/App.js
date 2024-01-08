ece622746a1796297dd7f42c6df1be01f9ef05c2
spontius415 <116679132+spontius415@users.noreply.github.com>
2024-01-08 16:14:06 -0500

    cleaned up formatting of file and added fields.

--- src/App.js
+++ src/App.js
@@ -1,82 +1,39 @@
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
-  let Weather = {
-    description: "Partly Cloudly",
-    temperature: 40,
-    wind: 2,
-    humidity: 45,
-  };
   return (
     <div className="App">
-      <div className="card">
-        <div className="card-body">
-          <form>
-            <div className="row">
-              <div className="col-8">
-                <div className="mb-3">
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="inputCity"
-                    placeholder="Enter City..."
-                    autocomplete="off"
-                  />
-                </div>
-              </div>
-              <div className="col-2">
-                <input type="submit" className="btn btn-light" value="🔍" />
-              </div>
-              <div className="col-2">
-                <input
-                  type="submit"
-                  className="btn btn-light"
-                  id="locationButton"
-                  value="📌"
-                />
-              </div>
-            </div>
-          </form>
-          <h1 id="currentCity">New York</h1>
-          <h3 id="currentDate">Monday, December 18, 3:48 PM</h3>
-          <h4>{Weather.description}</h4>
-          <div className="row currentWeather">
-            <div className="col-6 currentWeatherImage">
-              <img
-                src="https://openweathermap.org/img/wn/01d.png"
-                className="forecastImage"
-                alt="Clear "
-                id="weatherIcon"
-              />
-            </div>
-            <div className="col-6 currentWeatherTemperature">
-              <span id="currentTemperature">{Weather.temperature}</span>
-              <a href="." id="fahrLink" className="active">
-                °F
-              </a>
-            </div>
-          </div>
-          <div className="row currentWeatherParametrs">
-            <div className="col-6">
-              Wind: <span id="currentWind">{Weather.wind}</span> km/h
-            </div>
-            <div className="col-6">
-              Humidity: <span id="currentHumidity">{Weather.humidity}</span>%
-            </div>
-          </div>
-          <div className="weather-forecast" id="forecast"></div>
-        </div>
-      </div>
-      <div>
-        <a
-          id="githubLink"
-          href="https://github.com/spontius415/weather-react-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-source code
-        </a>
-        built by Sarah Pontius
+      <div className="container">
+        <Weather defaultCity="New York" />
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/spontius415"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Sarah Pontius
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/spontius415/weather-react-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://meek-cajeta-34285b.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
       </div>
     </div>
   );
